bbfbc5f4522213f490ecc9dd5319a338a72bdfdb
Denys Pozniak <denys.pozniak@gmail.com>
2024-11-25 21:29:08 +0100

    testing kemi

---
+++ kamailio/base/kamailio3.py
@@ -0,0 +1,30 @@
+import sys
+import KSR as KSR
+
+def mod_init():
+    KSR.info("===== from Python mod init\n")
+    dumpObj(KSR)
+    return kamailio()
+
+def ksr_request_route():
+    KSR.sl.sl_send_reply(200, "Ok --$HN(n)")
+    KSR.xlog.xwarn(" start debug me \n")
+
+    try:
+        ip = requests.get('https://api.ipify.org').text
+    except:
+        ip = "Failed to resolve"
+
+    if ksr_route_reqinit()==-255 :
+        return 1
+
+    return 1
+
+def ksr_route_reqinit():
+    KSR.set_reply_no_connect()
+    KSR.force_rport()
+    return 1
+
+def dumpObj(obj):
+    for attr in dir(obj):
+        KSR.warn("obj.%s = %s\n" % (attr, getattr(obj, attr)))
